25173c325640ed8cfee4f1213fa47fb93ed9bc0a
Amogh Rijal <xamoghx@gmail.com>
2022-11-19 20:43:49 +0545

    Introspects and deserealizes

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,35 @@
-use reqwest::{Client, Method};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
-type Error = Box<dyn std::error::Error>;
-type Result<T, E = Error> = std::result::Result<T, E>;
+#[derive(Serialize, Deserialize, Debug)]
+struct GETAPIResponse {
+    origin: String,
+}
 
-async fn introspect(url: &str) -> Result<()> {
-    let client = Client::new();
-    let operation_name = "IntrospectionQuery";
-    let query = "\n    query IntrospectionQuery {\n      __schema {\n        \n        queryType { name }\n        mutationType { name }\n        subscriptionType { name }\n        types {\n          ...FullType\n        }\n        directives {\n          name\n          description\n          \n          locations\n          args {\n            ...InputValue\n          }\n        }\n      }\n    }\n\n    fragment FullType on __Type {\n      kind\n      name\n      description\n      \n      fields(includeDeprecated: true) {\n        name\n        description\n        args {\n          ...InputValue\n        }\n        type {\n          ...TypeRef\n        }\n        isDeprecated\n        deprecationReason\n      }\n      inputFields {\n        ...InputValue\n      }\n      interfaces {\n        ...TypeRef\n      }\n      enumValues(includeDeprecated: true) {\n        name\n        description\n        isDeprecated\n        deprecationReason\n      }\n      possibleTypes {\n        ...TypeRef\n      }\n    }\n\n    fragment InputValue on __InputValue {\n      name\n      description\n      type { ...TypeRef }\n      defaultValue\n      \n      \n    }\n\n    fragment TypeRef on __Type {\n      kind\n      name\n      ofType {\n        kind\n        name\n        ofType {\n          kind\n          name\n          ofType {\n            kind\n            name\n            ofType {\n              kind\n              name\n              ofType {\n                kind\n                name\n                ofType {\n                  kind\n                  name\n                  ofType {\n                    kind\n                    name\n                  }\n                }\n              }\n            }\n          }\n        }\n      }\n    }\n  ";
-    let body = format!("operationName={}&query={}", operation_name, query);
-    let request = client.request(Method::POST, url).body(body);
-    let res = request.send().await?;
-    println!("{}", res.status());
-
-    let body = res.bytes().await?;
-
-    let v = body.to_vec();
-    let s = String::from_utf8_lossy(&v);
-    println!("response: {} ", s);
-
-    Ok(())
+#[derive(Serialize, Deserialize, Debug)]
+struct JSONResponse {
+    json: HashMap<String, String>,
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    introspect("http://localhost:7200/api/graphql").await?;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut map = HashMap::new();
+    map.insert("operationName", "IntrospectionQuery");
+    map.insert("query", "\n    query IntrospectionQuery {\n      __schema {\n        \n        queryType { name }\n        mutationType { name }\n        subscriptionType { name }\n        types {\n          ...FullType\n        }\n        directives {\n          name\n          description\n          \n          locations\n          args {\n            ...InputValue\n          }\n        }\n      }\n    }\n\n    fragment FullType on __Type {\n      kind\n      name\n      description\n      \n      fields(includeDeprecated: true) {\n        name\n        description\n        args {\n          ...InputValue\n        }\n        type {\n          ...TypeRef\n        }\n        isDeprecated\n        deprecationReason\n      }\n      inputFields {\n        ...InputValue\n      }\n      interfaces {\n        ...TypeRef\n      }\n      enumValues(includeDeprecated: true) {\n        name\n        description\n        isDeprecated\n        deprecationReason\n      }\n      possibleTypes {\n        ...TypeRef\n      }\n    }\n\n    fragment InputValue on __InputValue {\n      name\n      description\n      type { ...TypeRef }\n      defaultValue\n      \n      \n    }\n\n    fragment TypeRef on __Type {\n      kind\n      name\n      ofType {\n        kind\n        name\n        ofType {\n          kind\n          name\n          ofType {\n            kind\n            name\n            ofType {\n              kind\n              name\n              ofType {\n                kind\n                name\n                ofType {\n                  kind\n                  name\n                  ofType {\n                    kind\n                    name\n                  }\n                }\n              }\n            }\n          }\n        }\n      }\n    }\n  ");
+    let client = reqwest::Client::new();
+
+    let resp = client
+        .post("http://localhost:7200/api/graphql")
+        .json(&map)
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    let json: serde_json::Value = serde_json::from_str(resp.as_str()).expect("JSON was not well-formatted");
+
+    println!("{:#?}", json);
+
     Ok(())
 }
+
