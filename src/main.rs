66b3ed5efb29e19041770484624318f04866a395
Amogh Rijal <xamoghx@gmail.com>
2022-11-20 15:53:38 +0545

    some progress ?

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
-struct Token<'a> {
-    word: &'a str,
+struct MutationToken {
+    word: String,
     line_number: i32,
+    mutation_variable: Option<String>,
 }
 
-impl Token<'_> {
+impl MutationToken {
     fn print(&self) -> () {
-        println!("{} {}", self.word, self.line_number);
+        println!(
+            "{} {} {:?}",
+            self.word, self.line_number, self.mutation_variable
+        );
     }
 }
 
@@ -17,38 +21,60 @@ fn read_file(file_path: &str) -> String {
     return file_data;
 }
 
-fn get_tokens(content: &String) -> Vec<Token> {
-    let mut tokens: Vec<Token> = Vec::new();
+fn create_tokens(content: &String) -> (Vec<MutationToken>, HashMap<i32, String>) {
+    let mut tokens: Vec<MutationToken> = Vec::new();
+    let mut mutation_variables: Vec<String> = Vec::new();
+    let mut content_by_line: HashMap<i32, String> = HashMap::new();
     for (i, line) in content.lines().enumerate() {
+        content_by_line.insert(i as i32, String::from(line));
         for word in line.split_whitespace() {
-            tokens.push(Token {
-                word,
-                line_number: i as i32,
-            });
-        }
-    }
-    tokens
-}
-
-fn get_mutation_queries_from_tokens(tokens: Vec<Token>) -> Vec<Token> {
-    let mut filtered_tokens: Vec<Token> = Vec::new();
-    for token in tokens {
-        match token.word {
-            t if t.starts_with("use") && t.ends_with("Mutation()") => {
-                println!("{}", token.word);
-                filtered_tokens.push(token);
+            match word {
+                t if t.starts_with("use") && t.ends_with("Mutation()") => {
+                    tokens.push(MutationToken {
+                        word: String::from(word),
+                        line_number: i as i32,
+                        mutation_variable: None,
+                    });
+                }
+                t if t.ends_with("MutationVariables") => {
+                    mutation_variables.push(String::from(word));
+                }
+                _ => {}
             }
-            _ => {}
         }
     }
-    filtered_tokens
+
+    let mut updated_tokens: Vec<MutationToken> = Vec::new();
+
+    for token in tokens {
+        let related_mutation_variable = mutation_variables.iter().find(|&x| {
+            *x.replace("MutationVariables", "")
+                == token
+                    .word
+                    .clone()
+                    .replace("use", "")
+                    .replace("Mutation()", "")
+        });
+        match related_mutation_variable {
+            Some(i) => updated_tokens.push(MutationToken {
+                word: token.word,
+                line_number: token.line_number,
+                mutation_variable: Some(i.to_string()),
+            }),
+            None => {}
+        }
+    }
+
+    (updated_tokens, content_by_line)
 }
 
 fn main() {
     let file_data = read_file("graphql.ts");
-    let tokens = get_tokens(&file_data);
-    let mutation_tokens = get_mutation_queries_from_tokens(tokens);
-    // for token in mutation_tokens {
-    //     token.print();
-    // }
+    let (tokens, map) = create_tokens(&file_data);
+
+    for token in tokens {
+        if token.mutation_variable.is_some() {
+            // find main query
+        }
+    }
 }
