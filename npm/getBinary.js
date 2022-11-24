bdc3bbbbf88f48244916d0032cc5776104233f2f
Amogh Rijal <xamoghx@gmail.com>
2022-11-24 09:52:13 +0545

    fix 4

--- npm/getBinary.js
+++ npm/getBinary.js
@@ -4,7 +4,7 @@ function getBinary() {
   var url =
     "https://github.com/xamogh/safe-urqlcodgen-mutations/releases/download/v1.0.0-aplha/safe-urqlcodgen-mutations.tar.gz";
   var name = "safe-urql-codgen-mutations";
-  return new Binary(url, name);
+  return new Binary(name, url);
 }
 
 module.exports = getBinary;
